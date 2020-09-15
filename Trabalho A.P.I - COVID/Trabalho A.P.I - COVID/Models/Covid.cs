6f2dbd1dcf3f2781e4d0577dfc9a1279e6007bbb
LucasGuimaraes1 <63862235+LucasGuimaraes1@users.noreply.github.com>
2020-09-15 16:04:10 -0300

    Add files via upload

---
+++ Trabalho A.P.I - COVID/Trabalho A.P.I - COVID/Models/Covid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabalho_A.P.I___COVID.Models
+{
+    public class Covid
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Sexo{ get; set; }
+        public string EstadoCivil { get; set; }
+        public string Comorbidade { get; set; }
+        public string DataRegistro { get; set; }
+
+
+    }
+}
